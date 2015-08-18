7e675cddecc50e2e08e494e5765a620d12d56868
shubDhond <sdhond.sd@gmail.com>
2015-08-18 16:53:23 -0400

    Error for form and Loading spinner.

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,6 +2,7 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
     console.log("Hello World from controller");
+    $scope.createPickUpError=false;
 
     $http.get("api/user_data").success(function (response) {
         console.log('User: ' + JSON.stringify(response));
