140416f4d5ba94b070d48a5baf2fedac011cf207
shubDhond <shubham.dhond@mail.utoronto.ca>
2015-08-23 23:01:55 -0400

    Implementing markers for the pickUp objects.

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,7 +2,9 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
     console.log("Hello World from controller");
-    $scope.createPickUpError=false;
+    $scope.createPickUpError = false;
+    $scope.createPickUpErrorText = '';
+    $scope.loading = false;
 
     $http.get("api/user_data").success(function (response) {
         console.log('User: ' + JSON.stringify(response));
@@ -14,21 +16,64 @@ myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
             $scope.pickUps = response;
             $scope.pickUp = '';
             console.log($scope.pickUps);
+            for(var pickUp in $scope.pickUps){
+                geocoder.geocode( { 'address': pickUp.location}, function(results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        var marker = new google.maps.Marker({
+                            map: map,
+                            position: results[0].geometry.location
+                        });
+                    } else {
+                        alert('Geocode was not successful for the following reason: ' + status);
+                    }
+                });
+            }
         });
     };
     refresh();
 
     $scope.createPickUp = function () {
-        var pickUp = {};
-        pickUp.host = $scope.user._id;
-        pickUp.sport = $scope.sportAdd;
-        pickUp.location = $scope.locationAdd;
-        pickUp.date = $scope.dateAdd.toDateString();
-        pickUp.time = $scope.timeAdd.toTimeString();
-
-        $http.post('/pickUps', pickUp).success(function (response) {
-            refresh();
-            console.log(response);
-        });
+        $scope.loading = true;
+        if (!$scope.user) {
+            $scope.createPickUpErrorText = 'Session timed out please log in again';
+            $scope.createPickUpError = true;
+            $scope.loading = false;
+        } else if (!$scope.sportAdd) {
+            $scope.createPickUpErrorText = 'Please enter a sport so others know what you plan to play';
+            $scope.createPickUpError = true;
+            $scope.loading = false;
+        } else if (!$scope.locationAdd) {
+            $scope.createPickUpErrorText = 'Please enter a location so others can find this pick up';
+            $scope.createPickUpError = true;
+            $scope.loading = false;
+        } else if (!$scope.dateAdd) {
+            $scope.createPickUpErrorText = 'Please enter a date so others know which day this pick up will take place';
+            $scope.createPickUpError = true;
+            $scope.loading = false;
+        } else if (!$scope.timeAdd) {
+            $scope.createPickUpErrorText = 'Please enter a date so others know when to show up';
+            $scope.createPickUpError = true;
+            $scope.loading = false;
+        } else {
+            var pickUp = {};
+            pickUp.host = $scope.user._id;
+            pickUp.sport = $scope.sportAdd;
+            pickUp.location = $('#location').val().toString();
+            pickUp.date = $scope.dateAdd.toDateString();
+            pickUp.time = $scope.timeAdd.toTimeString();
+            $http.post('/pickUps', pickUp)
+                .success(function (response) {
+                    refresh();
+                    console.log(response);
+                    $('.add-pickUp').css({'display': 'none'});
+                    $('.overlay').css({'display': 'none'});
+                    $scope.loading = false;
+                })
+                .error(function(data,status){
+                    $scope.createPickUpErrorText = ''+status;
+                    $scope.createPickUpError = true;
+                    $scope.loading = false;
+                });
+        }
     };
 }]);
